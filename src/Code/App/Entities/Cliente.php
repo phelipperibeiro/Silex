172fa0a2f931575aa3c1259aa6cfb99fd6a0e588
Felipe Andrade <phelipperibeiro@hotmail.com>
2018-04-18 15:07:38 -0300

    add entity cliente para o doctrine

--- src/Code/App/Entities/Cliente.php
+++ src/Code/App/Entities/Cliente.php
@@ -2,12 +2,29 @@
 
 namespace Code\App\Entities;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="clientes")
+ */
 class Cliente
 {
+    /**
+     * @ORM\id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
     private $cliente;
     
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
     private $email;
     
     function getId()
